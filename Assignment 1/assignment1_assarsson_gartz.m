9490e4b82e8e0d3ca39b6b297824ffc260c273ee
Fabian Assarsson <fabian.assarson@icloud.com>
2018-03-26 09:06:49 +0200

    add helper files and create working file

---
+++ Assignment 1/assignment1_assarsson_gartz.m
@@ -0,0 +1,6 @@
+% Information: This program follows the instruction for lab 1 in the Masters Course
+% of Deep Learning at the Royal Institute of Technology. It is a multi-class one-layer
+% neural network trained with mini-batch gradient descent on both a cross-entropy-loss
+% against a softmax output, as well as a SVM multi-class loss.
+% Authors: Fabian Assarsson & Madeleine Gartz
+% Date: 2018-03-25
