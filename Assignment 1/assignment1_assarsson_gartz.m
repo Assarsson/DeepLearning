a8c2ccf1470065a12e0214ae34eed56636b8da64
Fabian Assarsson <fabian.assarson@icloud.com>
2018-03-26 09:41:21 +0200

    Add changes to image load

--- Assignment 1/assignment1_assarsson_gartz.m
+++ Assignment 1/assignment1_assarsson_gartz.m
@@ -4,3 +4,10 @@
 % against a softmax output, as well as a SVM multi-class loss.
 % Authors: Fabian Assarsson & Madeleine Gartz
 % Date: 2018-03-25
+
+% First we start with looking at some of the images from the dataset:
+addpath Datasets/cifar-10-batches-mat/; % adds our path to data
+A = load('data_batch_1.mat'); % loads a saved datafile with examples as rows in A
+I = reshape(A.data', 32, 32, 3, 10000); % We reshape it into a tensor for the montage-function
+I = permute(I, [2, 1, 3, 4]); % We permute the first and second axis
+montage(I(:,:,:,1:500), 'Size', [5,5]); % We display it with our helper function
