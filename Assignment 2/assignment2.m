347de9d4207d5fd1c82647b5d9ff1fa866b94cc3
Fabian Assarsson <fabian.assarson@icloud.com>
2018-04-15 17:09:06 +0200

    Add cell array Initializer function

--- Assignment 2/assignment2.m
+++ Assignment 2/assignment2.m
@@ -4,5 +4,9 @@ addpath Datasets/cifar-10-batches-mat/;
 [Xtrain, Ytrain, ytrain, Ntrain] = LoadBatch('data_batch_1.mat');
 [Xval, Yval, yval, Nval] = LoadBatch('data_batch_1.mat');
 [Xtest, Ytest, ytest, ntest] = LoadBatch('test_batch.mat');
-
+d = rows(Xtrain);
+N = Ntrain;
+K = rows(Ytrain);
+hiddenNodes = 50;
 [Xtrain, mean_of_Xtrain] = Preprocess(Xtrain);
+Theta = Initialize(K, d, hiddenNodes);
