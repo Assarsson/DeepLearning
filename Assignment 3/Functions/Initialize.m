17c9ea0b01a9f2b523f0216ece810d2265475a17
Fabian Assarsson <fabian.assarson@icloud.com>
2018-04-29 16:27:40 +0200

    Generalize initialize function

--- Assignment 3/Functions/Initialize.m
+++ Assignment 3/Functions/Initialize.m
@@ -1,39 +1,37 @@
-function [W, b] = Initialize(K, d, hiddenNodes, initType)
+function [W, b] = Initialize(d, layerData, initType)
   % Initialize creates our W and b cells and populates them with random values.
   % It either utilizes a random gaussian prior on the parameters or a Xavier prior.
   % The role of the Xavier prior is to keep X and and W*X equivariant, to increase
   % stability of the model and increase speed in our iterations.
   % INPUT:
-  %   K -- The number of classes for our problem
   %   d -- The dimensionality of our input X.
+  %   layerData -- A vector containing the number of nodes in each layer, including last.
   %   initType -- Optional variable that can invoke xavier-initialization.
   %
   % OUTPUT:
-  %   W -- A populated weight matrix of size (K, d)
-  %   b -- A populated bias vector of size (K, 1)
+  %   W -- A populated weight cell array of size (layers, 1)
+  %   b -- A populated bias cell array of size (layers, 1)
   % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-  W = cell(2,1);
-  b = cell(2,1);
-  W1 = double(randn(hiddenNodes,d));
-  W2 = double(randn(K, hiddenNodes));
-  b1 = double(zeros(hiddenNodes,1));
-  b2 = double(zeros(K,1));
   mu = 0;
   variance = 0.000001;
-  if nargin < 4
-    variance = 0.000001;
-  elseif (initType == 'xavier')
-    variance = 1/d;
-  elseif (initType == 'norand')
-    variance = 0.1;
-    W = double(ones(K,d));
-    b = double(ones(K,1));
-  end
-  W1 = W1*sqrt(variance) + mu;
-  W2 = W2*sqrt(variance) + mu;
-  W(1,1) = W1;
-  b(1,1) = b1;
-  W(2,1) = W2;
-  b(2,1) = b2;
+  layers = length(layerData);
+  W = cell(layers,1);
+  b = cell(layers,1);
 
+  W1 = double(randn(layerData(1), d));
+  b1 = double(zeros(layerData(1), 1));
+  W(1, 1) = W1;
+  b(1, 1) = b1;
+  for layer = 2:layers
+    Weight = double(randn(layerData(layer), layerData(layer-1)));
+    bias = double(zeros(layerData(layer), 1));
+    W(layer, 1) = Weight;
+    b(layer, 1) = bias;
+  endfor
+  if (initType == 'xavier')
+    variance = 1/d;
+  end
+  for layer = 2:layers
+    W(layer, 1) = W{layer, 1}*sqrt(variance) + mu;
+  endfor
 endfunction
---
+++ Assignment 3/assignment3.m
@@ -0,0 +1,4 @@
+addpath Functions/
+
+layerData = [50, 30, 30, 10];
+[W, b] = Initialize(12, layerData, 'xavier');
