4a39af97a772cbe358da8760e403c990bbbd803c
Fabian Assarsson <fabian.assarson@icloud.com>
2018-04-29 19:46:34 +0200

    Generalize Gradient calculations

--- Assignment 3/Functions/ComputeGradients.m
+++ Assignment 3/Functions/ComputeGradients.m
@@ -2,7 +2,7 @@ function [grad_b,grad_W] = ComputeGradients(X, Y, cache, W, b, N, lambda)
   % ComputeGradients computes the gradients of W and b as defined by differentiating
   % the cost function with respects to the node in the computational graph and traversing it
   % likeso, until we reach the parameter variable of interest. As we have a 2-layer network
-  % this results in us needing to update DL/DW1, DL/DW2, DL/Db1 and DL/Db2. 
+  % this results in us needing to update DL/DW1, DL/DW2, DL/Db1 and DL/Db2.
   % INPUT:
   %   X -- The current data batch of size (d, N_batch)
   %   Y -- The current one-hot label representation of size (K, N_batch) (true distribution)
@@ -16,40 +16,29 @@ function [grad_b,grad_W] = ComputeGradients(X, Y, cache, W, b, N, lambda)
   %   grad_W -- The gradient w.r.t W of size (K, d)
   %   grad_b -- The gradient w.r.t b of size (K, 1)
   % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-  grad_W = cell(2,1);
-  grad_b = cell(2,1);
-  grad_W1 = zeros(size(W{1,1}));
-  grad_b1 = zeros(size(b{1,1}));
-  grad_W2 = zeros(size(W{2,1}));
-  grad_b2 = zeros(size(b{2,1}));
-  h = cache{2,1};
-  W2 = W{2,1};
-  W1 = W{1,1};
-  s1 = cache{1,1};
-  P = cache{4,1};
+  layers = length(W);
+  grad_W = cell(layers,1);
+  grad_b = cell(layers,1);
+  grad_W = cellfun(@(x, y) zeros(size(y)), grad_W, W, 'UniformOutput', false);
+  grad_b = cellfun(@(x, y) zeros(size(y)), grad_b, b, 'UniformOutput', false);
+  P = cache{layers*2, 1};
+
   for i=1:N
     Xi = X(:,i);
     Yi = Y(:,i);
     Pi = P(:,i);
-    hi = h(:,i);
-    s1i = s1(:,i);
-    g = -Yi'*(diag(Pi) - Pi*Pi')/(Yi'*Pi); % We missed a f****** minus sign.
-    grad_b2 += g';
-    grad_W2 += g'*hi';
-    g = g*W2;
-    g = g*diag(s1i > 0);
-    grad_b1 += g';
-    grad_W1 += g'*Xi';
+    g = -(Yi-Pi)'; % We missed a f****** minus sign.
+    for layer = layers:-1:2
+      hi = cache{layer*2-2,1}(:,i);
+      si = cache{layer*2-3,1}(:,i);
+      grad_b{layer, 1} += g';
+      grad_W{layer, 1} += g'*hi'+2*lambda*W{layer,1};
+      g = g*W{layer, 1};
+      g = g*diag(si > 0);
+    endfor
+    grad_b{1, 1} += g';
+    grad_W{1, 1} += g'*Xi';
   end
-
-  grad_b1 /= N;
-  grad_W1 /= N;
-  grad_b2 /= N;
-  grad_W2 /= N;
-  grad_W1 += 2*lambda*W1;
-  grad_W2 += 2*lambda*W2;
-  grad_W(1,1) = grad_W1;
-  grad_b(1,1) = grad_b1;
-  grad_W(2,1) = grad_W2;
-  grad_b(2,1) = grad_b2;
+  grad_W = cellfun(@(x) x/N, grad_W, 'UniformOutput', false);
+  grad_b = cellfun(@(x) x/N, grad_b, 'UniformOutput', false);
 endfunction
--- Assignment 3/assignment3.m
+++ Assignment 3/assignment3.m
@@ -1,13 +1,14 @@
 addpath Functions/
-
+addpath Helpfunctions/
 [X, Y, y, N] = LoadBatch('data_batch_1.mat');
 d = rows(X);
 K = rows(Y);
-N = 10;
-X = X(:,1:N);
+N = 20;
+d = 200;
+X = X(1:d,1:N);
 Y = Y(:,1:N);
 y = y(1:N,:);
-layerData = [50, K];
+layerData = [10,10,10, K];
 [W, b] = Initialize(d, layerData, 'gaussi');
 [Wm, bm] = InitializeMomentum(W, b);
 disp('Displaying Weights');
@@ -19,3 +20,12 @@ disp('displaying cost');
 J = ComputeCost(X, Y, W, b, N, 0)
 disp('displaying accuracy');
 acc = ComputeAccuracy(X, y, W, b, N)
+disp('test gradients');
+[grad_b, grad_W] = ComputeGradients(X, Y, cache, W, b, N, 0);
+[grad_b_num, grad_W_num] = ComputeGradsNumSlow(X, Y, W, b, N, 0, 1e-5);
+disp('analytical');
+disp(grad_b{2,1});
+disp('numerical');
+disp(grad_b_num{2,1});
+cellfun(@(x, y) GradChecker(x, y), grad_b, grad_b_num, 'UniformOutput', false)
+cellfun(@(x, y) GradChecker(x, y), grad_W, grad_W_num, 'UniformOutput', false)
