505fcd6ab2fd33dcb2414767e6df818a29dd66ab
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-06 15:22:32 +0200

    Change arguments to compensate for new Evaluation-function

--- Assignment 3/BatchNormTest.m
+++ Assignment 3/BatchNormTest.m
@@ -5,8 +5,8 @@ d = rows(X);
 K = rows(Y);
 
 %% change parameters here (i.e. dimensionality of operations)
-d = 100;
-N = 3;
+d = 20;
+N = 2;
 %Reshape the data
 X = X(1:d,1:N);
 Y = Y(:,1:N);
@@ -16,17 +16,18 @@ y = y(1:N,:);
 layerData = [50, K]; %The same as in assignment 2
 lambda = 0;
 [W, b] = Initialize(d, layerData, 'gaussi');
-[cache, mus, vs] = EvaluateClassifier(X, W, b);
-[grad_b, grad_W] = ComputeGradients(X, Y, cache, mus, vs, W, b, N, lambda);
+[P, S,Shat, H, mus, vs] = EvaluateClassifier(X, W, b);
+[grad_b, grad_W] = ComputeGradients2(X, Y, P,S,Shat,H mus, vs, W, b, N, lambda);
 [grad_b_n, grad_W_n] = ComputeGradsNumSlow(X, Y, W, b, N, lambda, 1e-5);
 
 disp('differences in the bias gradients: ');
 cellfun(@(x, y) disp(GradChecker(x, y)), grad_b, grad_b_n, 'UniformOutput', false);
+disp('differences in the weight gradients: ');
+cellfun(@(x, y) disp(GradChecker(x, y)), grad_W, grad_W_n, 'UniformOutput', false);
+
 disp('size of BNcache parameters: ');
 cellfun(@(x) disp(size(x)), mus, 'UniformOutput', false);
 disp('size of b');
 cellfun(@(x) disp(size(x)), grad_b, 'UniformOutput', false);
 disp('size of W');
 cellfun(@(x) disp(size(x)), grad_W, 'UniformOutput', false);
-disp('value of b');
-cellfun(@(x) disp(x), grad_b, 'UniformOutput', false);
