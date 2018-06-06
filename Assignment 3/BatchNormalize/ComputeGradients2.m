3f2b52c5d5153ef348c3030e0a5e20ab1846a21c
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-06 16:07:04 +0200

    rewrite entire gradient function

---
+++ Assignment 3/BatchNormalize/ComputeGradients2.m
@@ -0,0 +1,57 @@
+function [grad_b,grad_W] = ComputeGradients(X, Y, P, S, Shat, H, mus, vs, W, b, N, lambda)
+  % ComputeGradients computes the gradients of W and b as defined by differentiating
+  % the cost function with respects to the node in the computational graph and traversing it
+  % likeso, until we reach the parameter variable of interest. As we have a 2-layer network
+  % this results in us needing to update DL/DW1, DL/DW2, DL/Db1 and DL/Db2.
+  % INPUT:
+  %   X -- The current data batch of size (d, N_batch)
+  %   Y -- The current one-hot label representation of size (K, N_batch) (true distribution)
+  %   P -- The current probability distribution calculated by our parametrized model.
+  %   W -- The current weight cell of size (2, 1) containing W1 and W2
+  %   b -- The current bias cell of size (2,1) containing b1 and b2
+  %   N -- The current batch size, i.e. N == N_batch
+  %   lambda -- The current scalar regularization parameter
+  %
+  % OUTPUT:
+  %   grad_W -- The gradient w.r.t W of size (K, d)
+  %   grad_b -- The gradient w.r.t b of size (K, 1)
+  % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+  layers = length(W);
+  grad_W = cell(layers,1);
+  grad_b = cell(layers,1);
+  grad_W = cellfun(@(x, y) double(zeros(size(y))), grad_W, W, 'UniformOutput', false);
+  grad_b = cellfun(@(x, y) double(zeros(size(y))), grad_b, b, 'UniformOutput', false);
+  g = cell(N, 1);
+  for i=1:N
+    Yi = Y(:,i);
+    Pi = P(:,i);
+    g{i} = -(Yi-Pi)'; % We missed a f****** minus sign.
+    hi = H{layers-1,1}(:,i);
+    si = S{layers-1,1}(:,i);
+    grad_b{layers,1} += g{i}';
+    grad_W{layers,1} += g{i}'*hi';
+    g{i} = g{i}*W{layers, 1};
+    g{i} = g{i}*diag(si > 0);
+  end
+  %grad_W{layers, 1} = grad_W{layers, 1}/N + 2*lambda*W{layers, 1};
+  %grad_b{layers, 1} = grad_b{layers,1}/N;
+  for layer = layers-1:-1:2
+    g = BatchNormBackPass2(g, S{layer}, mus{layer}, vs{layer}, N);
+    h = H{layer-1,1};
+    for i = 1:N
+      grad_b{layer, 1} += g{i}';
+      grad_W{layer, 1} += g{i}'*h(:,i)'; %+ 2*lambda*W{layer,1};
+      g{i} = g{i}*W{layer, 1};
+      si = S{layer-1,1}(:,i);
+      g{i} = g{i}*diag(si > 0);
+    endfor
+  endfor
+  g = BatchNormBackPass2(g, S{1}, mus{1}, vs{1}, N);
+  for i = 1:N
+    grad_b{1, 1} += g{i}';
+    grad_W{1, 1} += g{i}'*X(:,i)';
+  endfor
+  grad_b = cellfun(@(x) x/N, grad_b, 'UniformOutput', false);
+  grad_W = cellfun(@(x, y) x/N + 2*lambda*y, grad_W, W, 'UniformOutput', false);
+
+endfunction
