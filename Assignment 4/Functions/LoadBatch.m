655ff26ed58536229940918696325cc4bb16c1aa
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-07 14:00:51 +0200

    Add LoadBatch-function

---
+++ Assignment 4/Functions/LoadBatch.m
@@ -0,0 +1,20 @@
+
+function bookData = LoadBatch(fileName)
+
+  % This function acts as a trivial .txt-load interface
+  % It takes in a filename, opens it as long as it's stored in /Datasets,
+  % dumps the data to a variable
+  % closes the file and returns the data.
+  %
+  % INPUT:
+  %   fileName -- string variable containing the full filename
+  %
+  % OUTPUT:
+  %   bookData -- A vector containing the entirety of the file
+  % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+  warning('off', 'all'); %Shuts down the filepath warnings
+  addpath Datasets/; %Adds the standard path for files
+  fid = fopen(fileName, 'r'); %Opens the file into fid
+  bookData = fscanf(fid, '%c'); %scans the entire file and dumps it to bookData
+  fclose(fid); %Closes the file
+endfunction %ends the function
