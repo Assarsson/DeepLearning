1139ef35b007e8a94bcff4da6bc177c3a08b4e03
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-07 16:52:30 +0200

    Add Preprocess function

---
+++ Assignment 4/Functions/Preprocess.m
@@ -0,0 +1,24 @@
+
+function [bookChars, cToIx, ixToC] = Preprocess(bookData)
+  % This function acts as a preprocessing step for the data
+  % It takes in the textdata and generates a vector of unique characters,
+  % and two maps between the indices and the characters for later use.
+  %
+  % INPUT:
+  %   bookData -- vector containing the full book
+  %
+  % OUTPUT:
+  %   bookChars -- A vector containing the unique characters in the book
+  %   xToIx -- a Map that maps from characters to their index
+  %   ixToC -- a Map that maps from indices to their character.
+  % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+  bookChars = unique(bookData);
+  cs = bookChars;
+  cToIx = struct();
+  ixToC = struct();
+  for c = cs
+    ix = find(cs == c);
+    cToIx.(c) = ix;
+    ixToC.(num2str(ix)) = c;
+  endfor
+endfunction
