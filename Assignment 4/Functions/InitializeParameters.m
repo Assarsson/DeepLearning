b675022b96b56b6f470e64b242456339b7f39b02
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-07 17:36:45 +0200

    Add x0 and h0 parameters

--- Assignment 4/Functions/InitializeParameters.m
+++ Assignment 4/Functions/InitializeParameters.m
@@ -1,8 +1,10 @@
-function RNN = InitializeParameters(K, m)
+function [RNN, x0, h0] = InitializeParameters(K, m)
   sigma = 0.01;
   RNN.b = zeros(m, 1);
   RNN.c = zeros(K, 1);
   RNN.U = randn(m, K)*sigma;
   RNN.W = randn(m, m)*sigma;
   RNN.V = randn(K, m)*sigma;
+  x0 = zeros(K, 1);
+  h0 = zeros(m, 1);
 endfunction
