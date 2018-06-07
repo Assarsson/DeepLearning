fb18de7516df012d3b93db31b3a1c3fcc027c720
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-07 17:37:14 +0200

    update test to accomodate for new parameters

--- Assignment 4/Tests/testInitializeParameters.m
+++ Assignment 4/Tests/testInitializeParameters.m
@@ -4,9 +4,11 @@
 %! addpath Functions/
 %! K = 10;
 %! m = 20;
-%! RNN = InitializeParameters(K, m);
+%! [RNN, x0, h0] = InitializeParameters(K, m);
 %! assert(size(RNN.b) == [m,1]);
 %! assert(size(RNN.c) == [K,1]);
 %! assert(size(RNN.U) == [m,K]);
 %! assert(size(RNN.W) == [m,m]);
 %! assert(size(RNN.V) == [K,m]);
+%! assert(size(x0) == [K, 1]);
+%! assert(size(h0) == [m, 1]);
