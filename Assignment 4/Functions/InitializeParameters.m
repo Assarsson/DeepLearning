308e02fe53817bbed50df6fb11e8de9bfac01d0d
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-08 13:02:29 +0200

    Add X and Y initialization

--- Assignment 4/Functions/InitializeParameters.m
+++ Assignment 4/Functions/InitializeParameters.m
@@ -1,4 +1,4 @@
-function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp)
+function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx)
   sigma = 0.01;
   RNN.b = zeros(hp.m, 1);
   RNN.c = zeros(K, 1);
@@ -11,4 +11,12 @@ function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp)
   h0 = zeros(hp.m, 1);
   X = zeros(K, hp.seqLength);
   Y = zeros(K, hp.seqLength);
+
+  %convert to one-hot encoding
+  Xchars = bookData(1:hp.seqLength);
+  Ychars = bookData(2:hp.seqLength+1);
+  for i = 1:hp.seqLength
+    X(cToIx.(Xchars(i)), i) = 1;
+    Y(cToIx.(Ychars(i)), i) = 1;
+  endfor
 endfunction
