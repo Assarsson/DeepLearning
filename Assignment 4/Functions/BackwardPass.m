ae061952225c4da53dfcad2804075137cb51bf03
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-08 16:53:38 +0200

    add bias gradients and fix init conditions

--- Assignment 4/Functions/BackwardPass.m
+++ Assignment 4/Functions/BackwardPass.m
@@ -10,29 +10,32 @@ function gradients = BackwardPass(RNN, X, Y, P, H, hp)
     gradients.(fields{field}) = zeros(size(RNN.(fields{field})));
   endfor
 
-  % the V-gradient and intermediate o-gradients.
+  % the V-gradient, c-gradient and intermediate o-gradients.
   for t = 1:hp.seqLength
     y = Y(:, t);
     p = P(:, t);
     h = H(:, t);
     g = -(y-p)';  %generate the g as per ushe.
     dO(t, :) = g; %save the ot's
+    gradients.c += g';
     gradients.V += g'*h';
   endfor
 
   % The 'recursive' calculation of intermediate h and a-gradients.
+  dH(hp.seqLength, :) = dO(hp.seqLength, :)*RNN.V;
+  dA(hp.seqLength, :) = dH(hp.seqLength, :)*diag(1-(H(:, hp.seqLength)).^2);
   for t = hp.seqLength-1:-1:1
-    dh = dO(t,:)*RNN.V + dA(t + 1, :)*RNN.W;
-    da = dh*diag(1-H(:,t).^2);
-    dH(t, :) = dh;
-    dA(t, :) = da;
+    dH(t, :) = dO(t,:)*RNN.V + dA(t + 1, :)*RNN.W;
+    dA(t, :) = dH(t,:)*diag(1-H(:,t).^2);
   endfor
 
   % The W and U gradients that are left
   for t = 1:hp.seqLength
     gradients.U += dA(t, :)'*X(:, t)';
+    gradients.b += dA(t, :)';
     if (t == 1)
-      gradients.W += dA(t, :)'*H(:,hp.seqLength)';
+      %gradients.W += dA(t, :)'*H(:,hp.seqLength)';
+      gradients.W += dA(t, :)'*zeros(hp.m, 1)';
     else
       gradients.W += dA(t, :)'*H(:, t-1)';
     endif
