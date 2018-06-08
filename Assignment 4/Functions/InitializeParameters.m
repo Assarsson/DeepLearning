6e4f8ec4e69b6396168d1f2ab624e73261a836b4
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-08 12:39:51 +0200

    Add X and Y initialization

--- Assignment 4/Functions/InitializeParameters.m
+++ Assignment 4/Functions/InitializeParameters.m
@@ -1,12 +1,14 @@
-function [RNN, x0, h0] = InitializeParameters(K, m)
+function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp)
   sigma = 0.01;
-  RNN.b = zeros(m, 1);
+  RNN.b = zeros(hp.m, 1);
   RNN.c = zeros(K, 1);
-  RNN.U = randn(m, K)*sigma;
-  RNN.W = randn(m, m)*sigma;
-  RNN.V = randn(K, m)*sigma;
+  RNN.U = randn(hp.m, K)*sigma;
+  RNN.W = randn(hp.m, hp.m)*sigma;
+  RNN.V = randn(K, hp.m)*sigma;
   RNN.K = K;
-  RNN.m = m;
+  RNN.m = hp.m;
   x0 = zeros(K, 1);
-  h0 = zeros(m, 1);
+  h0 = zeros(hp.m, 1);
+  X = zeros(K, hp.seqLength);
+  Y = zeros(K, hp.seqLength);
 endfunction
