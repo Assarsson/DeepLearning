42b45ab5816bbbb7c056ba8d064ea4660c2d3dc3
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-08 16:46:40 +0200

    Adapt Numerical Gradient Checker

--- Assignment 4/Helpfunctions/ComputeGradsNum.m
+++ Assignment 4/Helpfunctions/ComputeGradsNum.m
@@ -1,12 +1,12 @@
-function num_grads = ComputeGradsNum(X, Y, RNN, h)
+function num_grads = ComputeGradsNum(X, Y, RNN, h, hp)
 
 for f = fieldnames(RNN)'
     disp('Computing numerical gradient for')
     disp(['Field name: ' f{1} ]);
-    num_grads.(f{1}) = ComputeGradNumSlow(X, Y, f{1}, RNN, h);
+    num_grads.(f{1}) = ComputeGradNum(X, Y, f{1}, RNN, h, hp);
 end
 
-function grad = ComputeGradNum(X, Y, f, RNN, h)
+function grad = ComputeGradNum(X, Y, f, RNN, h, hp)
 
 n = numel(RNN.(f));
 grad = zeros(size(RNN.(f)));
@@ -14,9 +14,8 @@ hprev = zeros(size(RNN.W, 1), 1);
 for i=1:n
     RNN_try = RNN;
     RNN_try.(f)(i) = RNN.(f)(i) - h;
-    l1 = ComputeLoss(X, Y, RNN_try, hprev);
+    l1 = ComputeLoss(RNN_try, X, Y, hprev, hp);
     RNN_try.(f)(i) = RNN.(f)(i) + h;
-    l2 = ComputeLoss(X, Y, RNN_try, hprev);
+    l2 = ComputeLoss(RNN_try, X, Y, hprev, hp);
     grad(i) = (l2-l1)/(2*h);
 end
-
