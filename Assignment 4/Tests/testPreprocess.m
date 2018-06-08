2ab9b673c2a2bedb6607a522645eff5ae9b20e55
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-08 11:43:06 +0200

    Update preprocess-test to account for K

--- Assignment 4/Tests/testPreprocess.m
+++ Assignment 4/Tests/testPreprocess.m
@@ -3,7 +3,8 @@
 %! fileName = 'goblet_book.txt';
 %! bookData = LoadBatch(fileName);
 %! assert(size(bookData) == [1, 1107545]);
-%! [bookChars, cToIx, ixToC] = Preprocess(bookData);
+%! [bookChars, cToIx, ixToC, K] = Preprocess(bookData);
+%! assert(length(bookChars) == K);
 %! assert(size(cToIx) == size(ixToC));
 %! assert(cToIx.(ixToC.('60')) == 60);
 %! assert(size(bookChars) == [1, 83]);
