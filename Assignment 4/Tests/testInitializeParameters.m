121edd5118a478bc312617900deb11bd588868d8
Fabian Assarsson <fabian.assarson@icloud.com>
2018-06-08 11:33:09 +0200

    Add new parameter value test

--- Assignment 4/Tests/testInitializeParameters.m
+++ Assignment 4/Tests/testInitializeParameters.m
@@ -10,5 +10,7 @@
 %! assert(size(RNN.U) == [m,K]);
 %! assert(size(RNN.W) == [m,m]);
 %! assert(size(RNN.V) == [K,m]);
+%! assert(RNN.K == K);
+%! assert(RNN.m == m);
 %! assert(size(x0) == [K, 1]);
 %! assert(size(h0) == [m, 1]);
