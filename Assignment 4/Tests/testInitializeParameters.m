4165904b6e74f9f32a99b061c44f785407838e0a
Fabian Assarsson <fabian.assarson@icloud.com>
2018-07-16 18:39:55 +0200

    Rewrite test to accomodate for function change

--- Assignment 4/Tests/testInitializeParameters.m
+++ Assignment 4/Tests/testInitializeParameters.m
@@ -7,7 +7,7 @@
 %! bookData = LoadBatch('goblet_book.txt');
 %! [bookChars, cToIx, ixToC, K] = Preprocess(bookData);
 %! hp.K = K;
-%! [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx);
+%! [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx, ixToC);
 %! assert(size(RNN.b) == [hp.m,1]);
 %! assert(size(RNN.c) == [K,1]);
 %! assert(size(RNN.U) == [hp.m,K]);
@@ -17,5 +17,5 @@
 %! assert(RNN.m == hp.m);
 %! assert(size(x0) == [K, 1]);
 %! assert(size(h0) == [hp.m, 1]);
-%! assert(size(X) == [K, hp.seqLength]);
-%! assert(size(Y) == [K, hp.seqLength]);
+%! assert(size(X) == [K, length(bookData)]);
+%! assert(size(Y) == [K, length(bookData)]);
