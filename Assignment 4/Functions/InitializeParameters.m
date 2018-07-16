19040b2a86e3535196313d40c866ae17e620cc14
Fabian Assarsson <fabian.assarson@icloud.com>
2018-07-16 18:39:05 +0200

    Fix dimensionality bug, add test-clause

--- Assignment 4/Functions/InitializeParameters.m
+++ Assignment 4/Functions/InitializeParameters.m
@@ -1,4 +1,4 @@
-function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx, ixToC)
+function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx, ixToC, testing)
   sigma = 0.01;
   RNN.b = zeros(hp.m, 1);
   RNN.c = zeros(K, 1);
@@ -10,16 +10,19 @@ function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx, ixTo
   RNN.cToIx = cToIx;
   RNN.ixToC = ixToC;
   [~, RNN.N] = size(bookData);
+  if (testing == true)
+    RNN.N = 25;
+  endif
   x0 = zeros(K, 1);
   h0 = zeros(hp.m, 1);
-  X = zeros(K, hp.seqLength);
-  Y = zeros(K, hp.seqLength);
+  X = zeros(RNN.K, RNN.N);
+  Y = zeros(RNN.K, RNN.N);
 
   %convert to one-hot encoding
   Xchars = bookData(1:hp.seqLength);
   Ychars = bookData(2:hp.seqLength+1);
-  for i = 1:hp.seqLength
-    X(cToIx.(Xchars(i)), i) = 1;
-    Y(cToIx.(Ychars(i)), i) = 1;
+  for i = 1:RNN.N
+    X(cToIx.(bookData(i)), i) = 1;
+    Y(cToIx.(bookData(i)), i) = 1;
   endfor
 endfunction
