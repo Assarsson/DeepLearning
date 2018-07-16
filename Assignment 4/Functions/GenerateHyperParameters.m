f353b2e61245e1ca6ddbffd49813b73b3965783e
Fabian Assarsson <fabian.assarson@icloud.com>
2018-07-16 12:32:01 +0200

    added hyper parameter

--- Assignment 4/Functions/GenerateHyperParameters.m
+++ Assignment 4/Functions/GenerateHyperParameters.m
@@ -6,17 +6,21 @@ function hp = GenerateHyperParameters(varargin)
     hp.m = 100;
     hp.eta = 0.1;
     hp.seqLength = 25;
+    hp.epsilon = 1e-8;
   elseif (length(varargin) == 1)
     hp.m = varargin{1};
     hp.eta = 0.1;
     hp.seqLength = 25;
+    hp.epsilon = 1e-8;
   elseif (length(varargin) == 2)
     hp.m = varargin{1};
     hp.eta = varargin{2};
     hp.seqLength = 25;
+    hp.epsilon = 1e-8;
   else
     hp.m = varargin{1};
     hp.eta = varargin{2};
     hp.seqLength = varargin{3};
+    hp.epsilon = 1e-8;
   end
 endfunction
