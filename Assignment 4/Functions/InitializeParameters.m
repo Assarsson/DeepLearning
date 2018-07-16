1452a80c79c985de313ca96a329ae8bb21b26cb1
Fabian Assarsson <fabian.assarson@icloud.com>
2018-07-16 14:11:19 +0200

    Add idx/char mappings to RNN-struct

--- Assignment 4/Functions/InitializeParameters.m
+++ Assignment 4/Functions/InitializeParameters.m
@@ -1,4 +1,4 @@
-function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx)
+function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx, ixToC)
   sigma = 0.01;
   RNN.b = zeros(hp.m, 1);
   RNN.c = zeros(K, 1);
@@ -7,6 +7,8 @@ function [RNN, x0, h0, X, Y] = InitializeParameters(K, hp, bookData, cToIx)
   RNN.V = randn(K, hp.m)*sigma;
   RNN.K = K;
   RNN.m = hp.m;
+  RNN.cToIx = cToIx;
+  RNN.ixToC = ixToC;
   [~, RNN.N] = size(bookData);
   x0 = zeros(K, 1);
   h0 = zeros(hp.m, 1);
