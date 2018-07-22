d0924d81e80aeac3041115f72caf1545ec160b0b
Fabian Assarsson <fabian.assarson@icloud.com>
2018-07-22 13:15:27 +0200

    Added file description

--- Assignment 4/Functions/ComputeCost.m
+++ Assignment 4/Functions/ComputeCost.m
@@ -1,3 +1,12 @@
 function J = ComputeCost(Y, P)
+  % This function computes the cost based on the cross-entropy loss function
+  %
+  % INPUT:
+  %   Y   -- A one-hot-matrix on character level for the output data
+  %   P   -- A probability matrix of the distributions for each character probability
+  %
+  % OUTPUT:
+  %   J   -- The cost for the current probability matrix.
+  % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
   J = -sum(log(sum(Y.*P, 1)));
 endfunction
