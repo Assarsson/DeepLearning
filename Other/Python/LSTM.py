c8e6f6ba0e310c3c4736744dbb4846ca71d55ddd
Fabian Assarsson <fabian.assarson@icloud.com>
2018-07-23 23:31:48 +0200

    Add save function and huge clean up

--- Other/Python/LSTM.py
+++ Other/Python/LSTM.py
@@ -2,46 +2,62 @@ import tensorflow as tf
 import tflearn
 import numpy as np
 from tflearn.data_utils import *
+import argparse
+import sys
 
 
 def readFiles(fileName, hp):
-    X, Y, idxs = textfile_to_semi_redundant_sequences("Datasets/"+fileName, seq_maxlen = hp['seqLength'], redun_step = 3)
-    return X, Y, idxs
+    try:
+        print("reading in files...")
+        X, Y, idxs = textfile_to_semi_redundant_sequences("Datasets/"+fileName, seq_maxlen = hp['seqLength'], redun_step = 3)
+        seed = random_sequence_from_textfile("Datasets/"+fileName, hp['seqLength'])
+        return X, Y, idxs, seed
+    except FileNotFoundError:
+        print("please choose a file that exists in the Dataset-folder")
+        sys.exit(1)
 def generateHyperParameters(eta = 0.1, m = 100, seqLength = 25, epochs = 10):
     hp = {'eta': eta, 'm': m, 'seqLength': seqLength, 'epochs': epochs}
     return hp
-print("reading in files...")
-fileName = 'goblet_book.txt'
-hp = generateHyperParameters()
-X, Y, idxs = readFiles(fileName, hp)
-#sequence generation
+def buildModel(hp, idxs):
+    print("building tensorflow model...")
+    net = tflearn.input_data(shape=[None,hp['seqLength'], len(idxs)])
+    print("added input layer")
+    net = tflearn.lstm(net, 128, return_seq = True)
+    print("added lstm layer")
+    net = tflearn.dropout(net, 0.3)
+    print("added dropout")
+    net = tflearn.lstm(net, 128)
+    print("added second lstm layer")
+    net = tflearn.dropout(net, 0.3)
+    net = tflearn.fully_connected(net,   len(idxs), activation='softmax')
+    print("added fully connected softmax")
+    net = tflearn.regression(net, optimizer = 'adam', loss = 'categorical_crossentropy')
+    print("added ADAM optimized cross entropy loss")
+    model = tflearn.SequenceGenerator(net, dictionary = idxs, seq_maxlen = hp['seqLength'], clip_gradients = 5.0)
+    print("created sequence generator")
+    print("model built!")
+    return model
+def parseFunction():
+    parser = argparse.ArgumentParser(description = "Process the integer parameters the LSTM-model")
+    parser.add_argument('--epochs', type = int, dest = 'epochs', default = 10, help = "defines the number of epochs")
+    parser.add_argument('--filename', type = str, dest = 'fileName', default = "goblet_book.txt", help = "define a file in the Datasets folder")
+    parser.add_argument('--genlen', type = int, dest = 'genlen', default = 200, help = "the length of the generated message")
+    arguments = parser.parse_args()
+    return arguments
+def sequenceGenerator(arguments, seed):
+    print("generating sequence...")
+    print("sequence with temperature 1.0")
+    print(model.generate(arguments.genlen, temperature = 1.0, seq_seed = seed))
+    print("sequence with temperature 0.5")
+    print(model.generate(arguments.genlen, temperature = 0.5, seq_seed = seed))
 
-print("building tensorflow model...")
-net = tflearn.input_data(shape=[None,hp['seqLength'], len(idxs)])
-print("added input layer")
-net = tflearn.lstm(net, 128, return_seq = True)
-print("added lstm layer")
-net = tflearn.dropout(net, 0.3)
-print("added dropout")
-net = tflearn.lstm(net, 128)
-print("added second lstm layer")
-net = tflearn.dropout(net, 0.3)
-net = tflearn.fully_connected(net,   len(idxs), activation='softmax')
-print("added fully connected softmax")
-net = tflearn.regression(net, optimizer = 'adam', loss = 'categorical_crossentropy')
-print("added ADAM optimized cross entropy loss")
-model = tflearn.SequenceGenerator(net, dictionary = idxs, seq_maxlen = hp['seqLength'], clip_gradients = 5.0)
-print("created sequence generator")
-print("model built!")
-print("fitting model...")
-model.fit(X, Y, n_epoch = 20)
-print("model fitted!")
-print("generating sequence...")
-seed = random_sequence_from_textfile("Datasets/"+fileName, hp['seqLength'])
-print("sequence with temperature 1.0")
-print(model.generate(200, temperature = 1.0, seq_seed = seed))
-print("sequence with temperature 0.5")
-print(model.generate(200, temperature = 0.5, seq_seed = seed))
-#[print(ixToC[np.where(x == 1)[0][0]]) for x in X[0:22]]
-#print("\n")
-#[print(ixToC[np.where(x == 1)[0][0]]) for x in Y[0:22]]
+if __name__ == "__main__":
+    arguments = parseFunction()
+    hp = generateHyperParameters()
+    X, Y, idxs, seed = readFiles(arguments.fileName, hp)
+    model = buildModel(hp, idxs)
+    print("fitting model...")
+    model.fit(X, Y, n_epoch = arguments.epochs)
+    print("model fitted!")
+    sequenceGenerator(arguments, seed)
+    model.save("Results/LSTM for {}.tfl".format(arguments.fileName))
